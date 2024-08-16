0403af67ff28699f8a87293f9e8178d83a46542d
Bruno Ricardo Machado <bruno.rm@grad.ufsc.br>
2024-08-16 11:53:26 -0300

    Atividade44_Bruno.java

--- src/a44/atividade44_Bruno.java
+++ src/a44/atividade44_Bruno.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 /*Modifique o problema anterior para que a quantidade de valores também seja fornecida pelo
 usuário.*/
-public class atividade44 {
+public class atividade44_Bruno {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
