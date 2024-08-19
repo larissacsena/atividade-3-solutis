484eb4ae734e73108f7f619c490b75537a9b00d8
Junior Aquino <93616016+GitAquino@users.noreply.github.com>
2024-08-18 22:16:26 -0300

    v2 - Todos exercicios realizados

--- src/a36/Atividade36.java
+++ src/a36/Atividade36.java
@@ -1,4 +1,20 @@
 package a36;
 
+import java.util.Scanner;
+
 public class Atividade36 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("N1 - Informe um número: ");
+        int n1 = sc.nextInt();
+        System.out.println("N2 - Informe outro número");
+        int n2 = sc.nextInt();
+
+        for (int i=n1; i<=n2; i++) {
+            if(i%2!=0){
+                System.out.println(i);
+            }
+        }
+    }
 }
